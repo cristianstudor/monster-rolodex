9c3fafed1d4c915a0cfb5def1fc8e302cfd06c36
cristianstudor <cristi_tcs@live.com>
2019-08-14 09:58:33 +0300

    Updates

---
+++ precache-manifest.0285e9715f5f56297afc67dcce20482d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06499e4c7c55f137e353cc2fd3512439",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6423774312c58ea1b5fe",
+    "url": "/monster-rolodex/static/css/main.ec684283.chunk.css"
+  },
+  {
+    "revision": "f7e71d2cca931e5dd749",
+    "url": "/monster-rolodex/static/js/2.89e16e46.chunk.js"
+  },
+  {
+    "revision": "6423774312c58ea1b5fe",
+    "url": "/monster-rolodex/static/js/main.d47b45a8.chunk.js"
+  },
+  {
+    "revision": "974ebdbc8482b2d59e77",
+    "url": "/monster-rolodex/static/js/runtime~main.cf73aa67.js"
+  }
+]);
